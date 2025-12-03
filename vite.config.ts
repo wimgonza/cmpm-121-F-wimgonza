9bbcbbe8b12a95e55d1e2c926fe0c5175dabaa70
inyoo403 <inyoo@ucsc.edu>
2025-12-03 12:09:54 +0000

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/CMPM-121-F/',
+  base: '/cmpm-121-F/',
   plugins: [react()],
 });
